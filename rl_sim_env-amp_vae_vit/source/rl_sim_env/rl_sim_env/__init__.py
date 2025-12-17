864798cd29149c195d5e1ba3d196be0421b984aa
ZhengLiu2002 <2488823262@qq.com>
2025-12-17 21:39:38 +0800

    修复多卡bug V3

--- rl_sim_env-amp_vae_vit/source/rl_sim_env/rl_sim_env/__init__.py
+++ rl_sim_env-amp_vae_vit/source/rl_sim_env/rl_sim_env/__init__.py
@@ -17,8 +17,14 @@ from .tasks import *
 from .ui_extension_example import *
 
 _env_root_override = os.environ.get("RL_SIM_ENV_ROOT_DIR")
-RL_SIM_ENV_ROOT_DIR = (
-    Path(_env_root_override).expanduser().resolve()
-    if _env_root_override
-    else Path(__file__).resolve().parents[4]
-)
+if _env_root_override:
+    RL_SIM_ENV_ROOT_DIR = Path(_env_root_override).expanduser().resolve()
+else:
+    _here_parents = Path(__file__).resolve().parents
+    _auto_root = None
+    for _candidate in _here_parents:
+        if (_candidate / "assets").is_dir():
+            _auto_root = _candidate
+            break
+    # fallback to previous heuristic if nothing found
+    RL_SIM_ENV_ROOT_DIR = _auto_root if _auto_root else _here_parents[4]
